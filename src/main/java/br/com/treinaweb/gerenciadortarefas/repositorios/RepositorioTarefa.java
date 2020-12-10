d7902bda5228b8d01651c9af10c7906ea3333705
NaionaraRamos <nramosmaceda@gmail.com>
2020-12-10 13:33:40 -0300

    Commit 1

---
+++ src/main/java/br/com/treinaweb/gerenciadortarefas/repositorios/RepositorioTarefa.java
@@ -0,0 +1,16 @@
+package br.com.treinaweb.gerenciadortarefas.repositorios;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.treinaweb.gerenciadortarefas.modelos.Tarefa;
+
+public interface RepositorioTarefa extends JpaRepository<Tarefa, Long>{
+	
+	//aula 4
+	@Query("SELECT t FROM Tarefa t WHERE t.usuario.email = :emailUsuario")
+	List<Tarefa> carregarTarefasPorUsuario(@Param("emailUsuario") String email);
+}
